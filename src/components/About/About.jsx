92a8ed8892e646a67643f0f12ff0dd0ca248b00b
Julián Tejada <111610260+TomateComunista@users.noreply.github.com>
2023-04-14 12:50:06 -0500

    rick_&_morty

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import style from "./About.module.css";
+
+const About = () => {
+    return(
+        <div className={style.container}>
+            <h1>Aún no está listo jóven</h1>
+            <p>Vuelva pronto :D</p>
+        </div>
+    )
+}
+
+export default About;
